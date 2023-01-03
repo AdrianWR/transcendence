d5a015dad264c82ddf35cf76657bb618f5465e14
Adrian Roque <adrian.w.roque@gmail.com>
2023-01-02 22:18:35 -0300

    chore: update Dockerfile in frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,17 +1,16 @@
 function Home(props) {
- 	return (
-      <h1>{props.message}</h1>
-	);
+  return (
+    <h1>{props.message}</h1>
+  );
 }
 export default Home
 
-export async function getServerSideProps(context){
-     
-    const response = await fetch(`http://backend:3000/user`);
-    const body = await response.text(); //
-    return {
-      props:{
-        message: body
-      }
+export async function getServerSideProps(context) {
+  const response = await fetch(`http://backend:8080/user`);
+  const body = await response.text(); //
+  return {
+    props: {
+      message: body
     }
+  }
 }
