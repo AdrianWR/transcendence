4bce1b8e212a9f453ad098e5ee2939087e76912c
Adrian Roque <adrian.w.roque@gmail.com>
2023-05-10 20:13:16 -0300

    chore: remove frontend-v1

--- backend/src/channels/dto/update-channel.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChannelDto } from './create-channel.dto';
-import { IsString, IsBoolean, IsOptional } from 'class-validator';
-
-export class UpdateChannelDto extends PartialType(CreateChannelDto) {}
